09560a557684de627cc22fa84546af856d0bcd7b
nahyuna <hyun_aaaa@naver.com>
2023-03-08 12:30:30 +0900

    0308-2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("당신의 나이 : " + age);
+
+        if (age <= 20 && age <= 34) {
+            System.out.println("청년");
+        } else if (age <= 18) {
+            System.out.println("미성년자");
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!\nHello world!\nHello world!\nHello world!");
+        System.out.println("안녕하세요!\n안녕하세요!\n안녕하세요!");
+
+        int age = 20;
+        System.out.println("당신의 나이 : " + age);
+
+        if (age <= 20 && age <= 34) {
+            System.out.println("청년");
+        } else if (age <= 18) {
+            System.out.println("미성년자");
+        }
     }
 }
